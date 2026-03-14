7d8799188da208323fd3302e1f019c528d5fcdee
boukely <maheryelo2@gmail.com>
2026-03-14 06:58:37 +0200

    update

--- app.js
+++ app.js
@@ -585,8 +585,8 @@ function renderDashboard() {
                     // ── Image du style (depuis GAS _imageUrl)
                     const imgUrl = r["_imageUrl"] || r["Image"] || "";
                     const imgBlock = imgUrl
-                        ? '<div class="dbs-sc-img-wrap"><img class="dbs-sc-img" src="' + imgUrl + '" alt="' + esc(r.Style || "") + '" loading="lazy" onerror="this.parentElement.classList.add('dbs-sc-img-error')"/></div>'
-                        : '<div class="dbs-sc-img-wrap dbs-sc-img-placeholder"><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' width='24' height='24'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z'/></svg></div>';
+                        ? '<div class="dbs-sc-img-wrap"><img class="dbs-sc-img" src="' + imgUrl + '" alt="' + esc(r.Style || "") + '" loading="lazy" onerror="this.parentElement.className+=\" dbs-sc-img-error\""/></div>'
+                        : '<div class="dbs-sc-img-wrap dbs-sc-img-placeholder"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/></svg></div>';
 
                     return '<div class="dbs-sc dbs-sc-v2" style="animation-delay:' + delay + 'ms" data-style="' + esc((r.Style || "").toLowerCase()) + '" data-desc="' + esc((r["Description"] || r["StyleDescription"] || "").toLowerCase()) + '" data-fabric="' + esc((r["Fabric Base"] || "").toLowerCase()) + '" data-client="' + esc((r.Client || "").toLowerCase()) + '">' +
                         imgBlock +
